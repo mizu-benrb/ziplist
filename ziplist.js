0aeaa6870c30ac1eaa75537c9bc23555a3d2fe3b
MizuBon <91563390+mizu-benrb@users.noreply.github.com>
2024-02-05 21:50:33 -1000

    Add ziplist.js + dependencies

---
+++ ziplist.js
@@ -0,0 +1,24 @@
+const zipList = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return;
+  }
+  let zippedArr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    zippedArr.push(arr1[i]);
+    zippedArr.push(arr2[i]);
+  }
+  return zippedArr;
+}
+
+const zipListTheSimpleWay = (arr1, arr2) => {
+  const zipSeparated = _.zip(arr1, arr2);
+  const zippedArr = [];
+  _.each(zipSeparated, (elemArr) => { zippedArr.concat(elemArr) });
+  return zippedArr;
+}
+
+let arr1 = ['a','b','c'];
+let arr2 = [1, 2, 3];
+
+console.log(zipList(arr1, arr2));
+console.log(zipListTheSimpleWay(arr1, arr2));
